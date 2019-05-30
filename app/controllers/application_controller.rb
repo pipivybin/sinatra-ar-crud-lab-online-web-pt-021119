7fd2dba330f31e39708a36e3b23258aecbaf70a6
Ming Duan <11203duan@gmail.com>
2019-05-30 00:17:47 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,49 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    erb :index
   end
+
+#create
+  get '/articles/new' do
+    erb :new #form
+  end
+
+  post '/articles' do
+    @article = Article.create(title: params[:title], content: params[:content])
+    redirect "/articles/#{@article.id}"
+  end
+
+# read
+  get '/articles' do
+    erb :index
+  end
+
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+#update
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+
+    erb :edit #form
+  end
+
+  patch '/articles/:id' do
+
+    @article = Article.find(params[:id]) #repetitive?
+
+    @article.update(title: params[:title], content: params[:content])
+
+    redirect "/articles/#{@article.id}" #can i do erb :show?
+  end
+
+#delete
+  delete '/articles/:id' do #does it have to add delete in the route?
+    Article.delete(params[:id])
+    redirect "/articles"
+  end
+
 end
